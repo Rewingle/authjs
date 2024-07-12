c9d2692246c9dcb0710531d3f3fd4d1157a90782
Mali <kul.mehmetali@yahoo.com>
2024-07-12 11:11:09 +0300

    fix

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,4 @@
-import { ModeSelect } from "@/components/modeSelect";
+import { ModeSelect } from "@/components/ModeSelect";
 import Navbar from "@/components/ui/navbar";
 
 interface ProtectedLayoutProps {
@@ -9,7 +9,7 @@ const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return (
     <>
       <Navbar />
-      <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-gray-900">
+      <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-gray-200">
 
         <ModeSelect />
         {children}
